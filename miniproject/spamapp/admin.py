9927b5646f72fc2d007f8d928b5e906905800726
HarikrishnaS2001 <91252841+HarikrishnaS2001@users.noreply.github.com>
2022-06-09 13:18:41 +0530

    Delete miniproject directory

--- miniproject/spamapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from spamapp.models import spammail
-
-# Register your models here.
-admin.site.register(spammail)
--- miniproject/spamapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from . models import spammail
-
-class email_form(forms.ModelForm):
-    class Meta:
-        model = spammail
-        fields = ('Email_Body',)
